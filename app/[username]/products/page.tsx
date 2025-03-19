60e33f5924e443401f41b17f95022fb6d6675a9a
Dhany Dwi Putra <dhany.dwi@nabatisnack.co.id>
2025-03-19 11:18:41 +0700

    Fixing metadata

--- app/[username]/products/page.tsx
+++ app/[username]/products/page.tsx
@@ -2,32 +2,40 @@ import axios from "axios";
 import ProductClient from "./client";
 import { Metadata } from "next";
 
-
-type Props = {
-  params: { id: string };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  // const product = await fetch(`https://api.example.com/products/${params.id}`).then((res) => res.json());
-
+export async function generateMetadata({ params }: { params: { username: string } }): Promise<Metadata> {
+  const { username } = await params
+  const salesData = await getUserData(username);
   return {
-    title: "Eko Toyota - Find Your Dream Car | Best Car Sales",
-    description: "Eko Toyota - Find the best car deals and professional consultation to get your dream vehicle today!",
-    keywords: "car sales, buy car, best cars, luxury cars, new cars, used cars",
+    title: `${salesData.fullname}`,
+    description: `${salesData.desc}`,
+    keywords: `${salesData.desc}`,
     authors: [{ name: "iniaga.id" }],
     openGraph: {
-      title: "Eko Toyota - Find Your Dream Car | Best Car Sales",
-      description: "Eko Toyota - Find the best car deals and professional consultation to get your dream vehicle today!",
-      images: ["https://media.licdn.com/dms/image/v2/C5603AQHrVI9o3JZw4w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1592757080803?e=2147483647&v=beta&t=6cWMwY2THK1cR77yfFRa_cB69xhy3bOAow6D9n6D0yU"],
+      title: `${salesData.fullname}`,
+      description: `${salesData.desc}`,
+      images: [`${salesData.url_image}`],
+      url: `${salesData.username}.iniaga.id`,
       type: "website",
     },
     twitter: {
       card: "summary_large_image",
-      title: "Eko Toyota - Find Your Dream Car | Best Car Sales",
-      description: "Eko Toyota - Find the best car deals and professional consultation to get your dream vehicle today!",
-      images: ["https://media.licdn.com/dms/image/v2/C5603AQHrVI9o3JZw4w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1592757080803?e=2147483647&v=beta&t=6cWMwY2THK1cR77yfFRa_cB69xhy3bOAow6D9n6D0yU"],
+      title: `${salesData.fullname}`,
+      description: `${salesData.desc}`,
+      images: [`${salesData.url_image}`],
     },
   };
+}
+
+async function getUserData(username: string) {
+  try {
+    const response = await axios.get(`https://apiniaga.zayyid.com/public/home/${username}`);
+    if (response.data.status === "success") {
+      return response.data.data;
+    }
+  } catch (err) {
+    console.error("Error fetching user data:", err);
+  }
+  return null;
 }
 
 export default async function Products({ params }: any) {
